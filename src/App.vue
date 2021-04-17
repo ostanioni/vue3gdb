7fd51216a7f3fe61b1f18392a69ea413f1f15206
ostanioni <ostanioni@gmail.com>
2021-04-17 20:56:59 +0700

    changed for test Icon component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
 <div>
+  <main-layout></main-layout>
+  <!--
   <hello-world msg="Hello, World!"></hello-world>
   <web-socket></web-socket>
+  -->
 </div>
 </template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import WebSocket from './components/WebSocket.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import WebSocket from './components/WebSocket.vue'
+import MainLayout from 'layouts/MainLayout.vue'
 
 const components = {
-  HelloWorld,
-  WebSocket
+  // HelloWorld,
+  // WebSocket,
+  MainLayout,
 }
 export default {
   components,
